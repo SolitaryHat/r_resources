8447650fc60f4733d9d8fcfd325fbc29213afe90
Nikita Ivanov <nikita.ivanov@omega-r.com>
2020-12-21 18:13:07 +0300

    initial commit + started images

---
+++ lib/flutter_resources.dart
@@ -0,0 +1,6 @@
+library flutter_resources;
+
+import 'package:build/build.dart';
+import 'package:flutter_resources/src/resources_generator.dart';
+
+Builder assetScannerBuilder(BuilderOptions builderOptions) => ResourcesBuilder();
